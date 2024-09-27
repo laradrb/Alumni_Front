b819a00c88afa2b2d2f8e56e7bd3b3e6abb0262d
pilimuino <pili.muino@gmail.com>
2024-09-27 09:35:30 +0200

    AL-32] feat: Al-44 LoginComponent: pages, routes, developed tests, testing libraries and scripts

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
-
+import Login from "../pages/Login";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +12,10 @@ export const router = createBrowserRouter([
             {
                 path: "/", 
                 element: <Home />,
+            },
+            {
+                path: "/login",
+                element: <Login/>,
             },
         ],
     },

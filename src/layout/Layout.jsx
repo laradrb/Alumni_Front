e76970c18dac1705665d16777acbd99e42adf71f
noemipeteilh <169151883+noemipeteilh@users.noreply.github.com>
2024-09-27 11:43:54 +0200

    Al-37-Navbar/NavbarCoders/Test

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavbarPublic from "../components/navbarPublic/Navbar-public";
+import NavbarCoders from "../components/navbarCoders/Navbar-coders";
 
 
 const Layout = () => {
     return (
         <>  
         <NavbarPublic/>
+        <NavbarCoders/>
         <main><Outlet /></main>      
         </>
     );

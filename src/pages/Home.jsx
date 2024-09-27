4378f5df97b6667893970fa8673cbc7176519f2e
laradrb <laradarriba@hotmail.com>
2024-09-27 12:35:07 +0200

    [AL-36] Feat: AL-40 OrangeCardComponent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "../components/firstdivision/FirstDivision";
+import OrangeCard from "../components/orangeCard/orangeCard";
 
 
 const Home = () => {
     return (
         <>
 <About/>
+<OrangeCard/>
         </>
     )
 }

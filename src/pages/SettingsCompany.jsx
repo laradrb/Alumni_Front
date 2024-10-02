2292bc195a74f11195c3935339e02aa45be422e1
pilimuino <pili.muino@gmail.com>
2024-10-02 09:48:52 +0200

    [AL-116]feat:AL-117 profile development settings companies and test

--- src/pages/SettingsCompany.jsx
+++ src/pages/SettingsCompany.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import CompaniesForm from '../components/profile/profileCompanies/settingsCompanies/CompaniesForm'
 
 const SettingsCompany = () => {
   return (
-    <div>
-      Settings company
-    </div>
+    <>
+      <CompaniesForm/>
+    </>
   )
 }
 
-export default SettingsCompany
+export default SettingsCompany;

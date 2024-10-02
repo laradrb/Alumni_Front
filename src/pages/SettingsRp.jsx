b40ea2c1924b819218d902959a1819bb6988f134
pilimuino <pili.muino@gmail.com>
2024-10-02 13:08:16 +0200

    [Al-63]feat: profile development settings Rp and test

---
+++ src/pages/SettingsRp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RpForm from '../components/profile/profileRp/settingsRp/RpForm';
+
+const SettingsRp = () => {
+    return (
+        <>
+        <RpForm/>
+        </>
+    )
+};
+
+export default SettingsRp;

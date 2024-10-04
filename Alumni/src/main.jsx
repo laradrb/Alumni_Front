558eefd665cf4295809eba7a3049b58a4a4968a8
Angelica perez <angelicanakaridperezpino@gmail.com>
2024-10-04 15:06:49 +0200

    [AL-70]feat:AL-74-I have deleted an alumni folder in my branch that was unnecessary

--- Alumni/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GlobalStyle from './styled/GlobalStyle'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <GlobalStyle />
-  </React.StrictMode>
-);

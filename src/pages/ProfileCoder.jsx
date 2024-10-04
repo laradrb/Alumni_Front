edbfb43505ed4ab1d3a634979df821b8373634ba
Viviana <acero.vivianapulido@gmail.com>
2024-10-04 15:35:32 +0200

    [AL-53]feat:AL-55-create-route

--- src/pages/ProfileCoder.jsx
+++ src/pages/ProfileCoder.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ProfileView from '../components/profile/profileCoder/viewCardSkills/ProfileView';
 
 const ProfileCoder = () => {
     return (
         <>
+        <ProfileView/>
         </>
     )
 };

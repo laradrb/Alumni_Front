18b4b5b092bb0d1745a77f3620f7eb26a6124c4c
AngelinaBassano <bassanoangelina2002@gmail.com>
2024-10-07 21:37:29 +0200

    [AL-62] feat:AL-96 Certificate Form

---
+++ src/components/profile/profileCoder/settingsCertificate/SettingsCertificateComponent.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import Input from '../../../atom/Input'; 
+import { CertificacionesContainer, InputContainer, ButtonContainer, Title, FormContainer, ImageUploadContainer} from './styledSettingsCertificateComponent'; 
+import GlobalStyle from '../../../../styled/GlobalStyle';
+
+
+const SettingsCertificate = () => {
+    const [formState, setFormState] = useState({
+        titulo: '',
+        escuela: '',
+        fechaInicio: '',
+        fechaFin: '',
+    });
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormState((prevState) => ({ ...prevState, [name]: value }));
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setImagePreview(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleSave = () => {
+        const certificateData = {
+            ...formState,
+            imagen: imagePreview,
+        };
+        
+    };
+
+    return (
+        <>
+            <GlobalStyle />
+            <CertificacionesContainer>
+                <Title>Crear Certificación</Title>
+                <FormContainer>
+                    <ImageUploadContainer>
+                        {imagePreview ? (
+                            <img src={imagePreview} alt="Vista previa" />
+                        ) : (
+                            <p>Subir Imagen</p>
+                        )}
+                        <input
+                            type="file"
+                            accept="image/*"
+                            onChange={handleImageChange}
+                        />
+                    </ImageUploadContainer>
+
+                    <InputContainer>
+                        <Input
+                            type="text"
+                            placeholder="Título"
+                            name="titulo"
+                            value={formState.titulo}
+                            onChange={handleInputChange}
+                        />
+                        <Input
+                            type="text"
+                            placeholder="Escuela"
+                            name="escuela"
+                            value={formState.escuela}
+                            onChange={handleInputChange}
+                        />
+                        <Input
+                            type="text"
+                            placeholder="Fecha de Inicio (dd/mm/aaaa)"
+                            name="fechaInicio"
+                            value={formState.fechaInicio}
+                            onChange={handleInputChange}
+                        />
+                        <Input
+                            type="text"
+                            placeholder="Fecha de Fin (dd/mm/aaaa)"
+                            name="fechaFin"
+                            value={formState.fechaFin}
+                            onChange={handleInputChange}
+                        />
+                    </InputContainer>
+                </FormContainer>
+                <ButtonContainer>
+                  
+                </ButtonContainer>
+            </CertificacionesContainer>
+        </>
+    );
+};
+
+export default SettingsCertificate;
---
+++ src/components/profile/profileCoder/settingsCertificate/styledSettingsCertificateComponent.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const CertificacionesContainer = styled.div`
+  border: 2px solid #FF6600;
+  border-radius: 10px;
+  padding: 1.25rem;  
+  width: 66.8125rem;  
+  height: 33.9375rem; 
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export const Title = styled.h2`
+  color: #FF4700; 
+  text-align: center; 
+  font-family: 'Poppins', sans-serif; 
+  font-size: 2rem; 
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  justify-content: center; 
+  align-items: flex-start; 
+  gap: 2rem; 
+  margin-top: 1.25rem; 
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column; 
+  gap: 0.625rem; 
+  width: 50%; 
+  padding-left: 1rem; 
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 1.25rem; 
+`;
+
+export const ImageUploadContainer = styled.div`
+  width: 17.24rem; 
+  height: 11.53rem; 
+  border: 2px dashed #ccc;
+  background-color: #f0f0f0; 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  overflow: hidden;
+  border-radius: 5px;
+  cursor: pointer; 
+
+  &:hover {
+    background-color: #e0e0e0; 
+  }
+
+  input {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    cursor: pointer; 
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; 
+    border-radius: 5px; 
+  }
+
+  p {
+    font-size: 1rem;
+    color: #666;
+    pointer-events: none; 
+  }
+`;

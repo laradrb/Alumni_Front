ed1caa2cc822d805a7a866afb7b015594735f773
laradrb <laradarriba@hotmail.com>
2024-10-07 22:57:35 +0200

    [AL-126] Feat: AL-128 Complete Settings Job Offer and Tests

--- src/components/profile/profileCompanies/completeFormOffer/CompleteFormOffer.test.jsx
+++ src/components/profile/profileCompanies/completeFormOffer/CompleteFormOffer.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import CompleteFormOffer from './CompleteForm';
+import CompleteFormOffer from './completeForm';
 
 describe('CompleteFormOffer Component', () => {
   it('renders the form title correctly', () => {

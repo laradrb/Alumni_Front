0ed113fea330d7e6c0d84e43ed05bdbd6c2e85d7
laradrb <laradarriba@hotmail.com>
2024-10-09 16:49:16 +0200

    [AL-36] Feat: AL-37 Updating routes and links

--- src/components/navbarCompanies/NavbarCompanies.jsx
+++ src/components/navbarCompanies/NavbarCompanies.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import LogoF5 from '../logo/Logo.jsx';
 import { Nav, NavLinks, StyledLink } from './styledNavbarCompanies.js';
+import { Link } from'react-router-dom';
 
 const NavbarCompanies = () => {
 return (
     <Nav>
     <NavLinks>
-        <StyledLink to="/aplications">APLICACIONES</StyledLink>
-        <StyledLink to="/profile">PERFIL</StyledLink>
+        <StyledLink to="/empresa/aplicaciones-oferta">APLICACIONES</StyledLink>
+        <StyledLink to="/empresa/perfil-empresa">PERFIL</StyledLink>
         <StyledLink to="/logout">CERRAR SESIÓN</StyledLink>
     </NavLinks>
+    <Link to="/home">
     <LogoF5/>
+    </Link>
     </Nav>
 );
 };
